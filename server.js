4f3662d015bbac39ba7359c3fe1b1cd68697115a
KiriZhu <kiribaty88@gmail.com>
2020-08-30 21:13:23 +0200

    expanded backend socket io events

--- server.js
+++ server.js
@@ -3,14 +3,32 @@ const io = require('socket.io')();
 // generate userlist displayed on the right in browser
 let userlist = [];
 
+function updateUserlist(action, sender) {
+  // add user to userlist
+  if (action === 'join') {
+    userlist.push({
+      username: sender,
+    });
+  }
+
+  // remove user from userlist
+  else if (action === 'leave') {
+    userlist.map((user, index) => {
+      if (user.username === sender) {
+        userlist.splice(index, 1);
+      }
+    });
+  }
+}
+
 // socket.io connection open
 io.on('connection', function (socket) {
   // join
   socket.on('chat join', function (msg) {
     //TODO: add user to userlist
-
+    updateUserlist('join', msg.sender);
     //TODO: send userlist
-
+    io.emit('chat users', userlist);
     // send join message
     io.emit('chat join', msg);
   });
@@ -18,9 +36,9 @@ io.on('connection', function (socket) {
   // leave
   socket.on('chat leave', function (msg) {
     //TODO: remove user from userlist
-
+    updateUserlist('leave', msg.sender);
     //TODO: send userlist
-
+    io.emit('chat users', userlist);
     // send leave message
     io.emit('chat leave', msg);
   });
